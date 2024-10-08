859a945413fa7f9fcdd3a225d04f5b18d5e99365
Wei Ji <23487320+weiji14@users.noreply.github.com>
2024-10-08 16:43:36 +1300

    Rename benchmark group and functions
    Set benchmark group name to `read_geotiff_50MB`, and benchmark function names to `u8_dtype`, `i16_dtype`, `f32_dtype`.

--- benches/read_geotiff.rs
+++ benches/read_geotiff.rs
@@ -11,17 +11,17 @@ fn read_geotiff(fpath: &str) -> TiffResult<()> {
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("throughput-example");
+    let mut group = c.benchmark_group("read_geotiff_50MB");
     group.throughput(Throughput::BytesDecimal(50e6 as u64)); // 50MB filesize
     group.measurement_time(Duration::from_secs(10));
 
-    group.bench_function("read_geotiff u8", move |b| {
+    group.bench_function("u8_dtype", move |b| {
         b.iter(|| read_geotiff("resources/byte_50m.tif"))
     });
-    group.bench_function("read_geotiff i16", move |b| {
+    group.bench_function("i16_dtype", move |b| {
         b.iter(|| read_geotiff("resources/int16_50m.tif"))
     });
-    group.bench_function("read_geotiff f32", move |b| {
+    group.bench_function("f32_dtype", move |b| {
         b.iter(|| read_geotiff("resources/float32_50m.tif"))
     });
     group.finish();
