16ddd6b21975fb0abeac2f0c3b60b1043891408e
Gunnar Schulze <gunnar.schulze@invenium.io>
2024-10-08 05:21:04 +0200

    chore: Directly initialize transform and inverse transform

--- src/coordinate_transform/affine_transform.rs
+++ src/coordinate_transform/affine_transform.rs
@@ -5,13 +5,14 @@ use crate::coordinate_transform::CoordinateTransform;
 
 impl CoordinateTransform {
     pub fn from_transformation_matrix(transformation_matrix: [f64; 16]) -> TiffResult<Self> {
-        let mut transform = [0f64; 6];
-        transform[0] = transformation_matrix[0];
-        transform[1] = transformation_matrix[1];
-        transform[2] = transformation_matrix[3];
-        transform[3] = transformation_matrix[4];
-        transform[4] = transformation_matrix[5];
-        transform[5] = transformation_matrix[7];
+        let transform = [
+            transformation_matrix[0],
+            transformation_matrix[1],
+            transformation_matrix[3],
+            transformation_matrix[4],
+            transformation_matrix[5],
+            transformation_matrix[7],
+        ];
 
         let det = transform[0] * transform[4] - transform[1] * transform[3];
         if det.abs() < 0.000000000000001 {
@@ -20,13 +21,14 @@ impl CoordinateTransform {
             )));
         }
 
-        let mut inverse_transform = [0f64; 6];
-        inverse_transform[0] = transform[4] / det;
-        inverse_transform[1] = -transform[1] / det;
-        inverse_transform[2] = (transform[1] * transform[5] - transform[2] * transform[4]) / det;
-        inverse_transform[3] = -transform[3] / det;
-        inverse_transform[4] = transform[0] / det;
-        inverse_transform[5] = (-transform[0] * transform[5] + transform[2] * transform[3]) / det;
+        let inverse_transform = [
+            transform[4] / det,
+            -transform[1] / det,
+            (transform[1] * transform[5] - transform[2] * transform[4]) / det,
+            -transform[3] / det,
+            transform[0] / det,
+            (-transform[0] * transform[5] + transform[2] * transform[3]) / det,
+        ];
 
         Ok(CoordinateTransform::AffineTransform {
             transform,
@@ -34,10 +36,7 @@ impl CoordinateTransform {
         })
     }
 
-    pub(super) fn transform_by_affine_transform(
-        transform: &[f64; 6],
-        coord: &Coord,
-    ) -> Coord {
+    pub(super) fn transform_by_affine_transform(transform: &[f64; 6], coord: &Coord) -> Coord {
         Coord {
             x: coord.x * transform[0] + coord.y * transform[1] + transform[2],
             y: coord.x * transform[3] + coord.y * transform[4] + transform[5],
