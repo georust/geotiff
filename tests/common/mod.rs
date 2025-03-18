b9e86274af127cee65d89c2f05e8d7f8646e3775
Vinicius <vinifreitas.d.a@gmail.com>
2025-03-18 18:38:19 -0300

    chore: roll back changes

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,18 +1,8 @@
-use std::error::Error;
 use std::fs::File;
 use std::path::Path;
 
 use geotiff::GeoTiff;
 
 pub fn read_geotiff<P: AsRef<Path>>(path: P) -> GeoTiff {
-    match GeoTiff::read(File::open(path).expect("File I/O error")) {
-        Ok(g) => g,
-        Err(e) => {
-            eprintln!("Error reading GeoTIFF: {}", e);
-            if let Some(source) = e.source() {
-                eprintln!("Caused by: {}", source);
-            }
-            panic!();
-        }
-    }
+    GeoTiff::read(File::open(path).expect("File I/O error")).expect("File I/O error")
 }
